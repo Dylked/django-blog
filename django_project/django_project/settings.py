def315a68998f7f34e822c1a4b85fddec6923bcd
dylked <dylked2011@gmail.com>
2024-09-01 09:45:37 +0200

    feat: Add profile page

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -126,6 +126,8 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 from django.contrib.auth import logout
 # Create your views here.
@@ -22,3 +23,7 @@ def register(request):
 def logout_view(request):
     logout(request)
     return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
