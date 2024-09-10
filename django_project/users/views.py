11af8c4c65893c384729421bbe2b22fa80f26819
dylked <dylked2011@gmail.com>
2024-09-10 22:30:19 +0200

    fix: Adjusting reset password functionality

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -37,7 +37,7 @@ def profile(request):
             return redirect('profile')
     else:
         u_form= UserUpdateForm(instance=request.user)
-        p_form=ProfileUpdateForm(instance=request.user.profile)
+        p_form= ProfileUpdateForm(instance=request.user.profile)
     context = {
         'u_form': u_form,
         'p_form': p_form
